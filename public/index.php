d49ec1f416bb471b7d5c9a337a6660bb37688d99
SANCHEZ nicolas <77881361+nicopierresanchez@users.noreply.github.com>
2021-10-20 17:03:00 +0200

    first

--- public/index.php
+++ public/index.php
@@ -5,4 +5,16 @@ require __DIR__ . "/../vendor/autoload.php";
 use Wcs\Hello;
 
 
-echo Hello::talk();
+//echo Hello::talk();
+
+use CowSay\Cow;
+
+$bessie = new Cow('Hello, Farm!');
+
+
+$bessie = new Cow('J\'aime faire CACA');
+$bessie->setEyes('oO')
+    ->setTongue('U')
+    ->setPoop('@@@ @@ @@@ @ @@@@ @@ @@ @ @@@ @@@@@')
+    ->setUdder('P');
+echo $bessie . PHP_EOL ;
